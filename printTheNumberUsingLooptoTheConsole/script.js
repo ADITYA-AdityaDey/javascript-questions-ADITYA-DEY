d8246c70d28b2f7d71f2caa204c401194aef0e44
Aditya Dey <deyaditya2004@gmail.com>
2024-09-09 02:35:13 +0530

    Added Print the numbers using loop

---
+++ printTheNumberUsingLooptoTheConsole/script.js
@@ -0,0 +1,25 @@
+// Print the numbers from 1 to 10 to the console using loop
+
+console.log("Using For Loop");
+
+for(let i=1; i<=10; i++) {
+    console.log(i);
+}
+
+console.log("Using while loop");
+
+let i = 1;
+while(i<=10) {
+    console.log(i);
+    i++;
+}
+
+console.log("Using do while loop");
+
+let j=1;
+
+do {
+    console.log(j);
+    j++;
+}
+while(j<=10);
